eb171e85a4e54425d8bf52c0df612b68a1c1a7cc
back and white <01backandwhite@gmail.com>
2026-03-06 22:15:08 +0100

    Configuracion del changelog

--- src/main/java/com/backandwhite/MicGatewayserviceApplication.java
+++ src/main/java/com/backandwhite/MicGatewayserviceApplication.java
@@ -3,10 +3,16 @@ package com.backandwhite;
 import com.backandwhite.common.configuration.annotation.EnableCoreApplication;
 import com.backandwhite.common.security.jwt.JwtProperties;
 import com.backandwhite.infrastructure.configuration.ServicesProperties;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @EnableCoreApplication
+@OpenAPIDefinition(servers = {
+        @Server(url = "https://mic-apigatewayservice-production.up.railway.app", description = "Production Server."),
+        @Server(url = "https://localhost:3030", description = "Local Server.")
+})
 @EnableConfigurationProperties({JwtProperties.class, ServicesProperties.class})
 public class MicGatewayserviceApplication {
 
